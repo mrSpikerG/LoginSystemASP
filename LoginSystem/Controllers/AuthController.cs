6418a21eab2e41766976804df1e3b47da5183267
thespiker <Fili_vw23@student.itstep.org>
2022-12-16 19:23:07 +0200

    Добавьте файлы проекта.

---
+++ LoginSystem/Controllers/AuthController.cs
@@ -0,0 +1,76 @@
+﻿using LoginSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using UAParser;
+
+namespace LoginSystem.Controllers {
+
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class AuthController : Controller {
+
+
+        public static List<AuthModel> Users { get; set; } = new List<AuthModel>();
+
+        [HttpPost(Name = "Register")]
+        public IActionResult Register(string login, string password, string mail) {
+
+            int loginSize = login.Length;
+            int passwordSize = password.Length;
+
+            if (loginSize <= 4 || loginSize > 12) {
+                return StatusCode(403);
+            }
+
+            if (passwordSize <= 4 || passwordSize > 16) {
+                return StatusCode(403);
+            }
+
+
+            try {
+                AuthModel user = Users.First(x => x.Login.Equals(login) && x.Mail.Equals(mail));
+            } catch (Exception ex) {
+                Users.Add(new AuthModel(login, password, mail));
+                return Ok("Ok");
+            }
+
+            return StatusCode(403);
+
+        }
+
+
+        [HttpPost(Name = "LoginByName")]
+        public IActionResult LoginbyName(string login, string password) {
+
+
+            if (!Users.Any(x => x.Login.Equals(login))) {
+                return StatusCode(403);
+            }
+
+            var user = Users.First(x => x.Login.Equals(login));
+            string shaPassword = AuthUtilController.ComputeSha256Hash(password);
+
+            if (user.Password.Equals(shaPassword)) {
+                return Ok("Ok");
+            }
+            return StatusCode(403);
+        }
+
+        [HttpPost(Name = "LoginByMail")]
+        public IActionResult LoginByMail(string mail, string password) {
+
+
+            if (!Users.Any(x => x.Mail.Equals(mail))) {
+                return StatusCode(403);
+            }
+
+            var user = Users.First(x => x.Mail.Equals(mail));
+            string shaPassword = AuthUtilController.ComputeSha256Hash(password);
+
+            if (user.Password.Equals(shaPassword)) {
+                return Ok("Ok");
+            }
+            return StatusCode(403);
+        }
+
+    }
+}
---
+++ LoginSystem/Models/AuthModel.cs
@@ -0,0 +1,17 @@
+﻿using LoginSystem.Controllers;
+
+namespace LoginSystem.Models {
+    public class AuthModel {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Mail { get; set; }
+        public DateTime TimeCreation { get; set; }
+
+        public AuthModel(string login, string password, string mail) {
+            this.Login = login;
+            this.Password = AuthUtilController.ComputeSha256Hash(password);
+            this.Mail = mail;
+            this.TimeCreation = DateTime.Now;
+        }
+    }
+}
